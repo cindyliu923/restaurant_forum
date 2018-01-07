0dfef4a853d8d01476b21550cb866ed85a05974b
Cindy Liu <cindyliu923@gmail.com>
2018-01-07 17:34:49 +0800

    setup routes for user index page and follow / unfollow function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
   devise_for :users
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
+
+  resources :followships, only: [:create, :destroy]
 
   resources :restaurants, only: [:index, :show] do
     resources :comments, only: [:create, :destroy]
