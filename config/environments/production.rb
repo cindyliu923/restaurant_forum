f5c6d7fa839d3153f9910512e1d70924624936c2
Cindy Liu <cindyliu923@gmail.com>
2018-06-17 14:51:40 +0800

    modify mailer setting for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,14 +64,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: "https://restaurant-forum-by-cindy.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "heroku.com",
+    :domain => "gmail.com",
     :authentication => :plain,
     :user_name => ENV["GMAIL_USERNAME"], #你的信箱
     :password => ENV["GMAIL_PASSWORD"], #信箱密碼 
